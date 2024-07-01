5ae7745e5461175717d7c6fb59d14b58af22e0e1
Amro Sati <amrosatti98@gmail.com>
2024-07-01 21:54:02 +0300

    task 1 fixed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 # Installs 'flask' from 'pip3'
 
-exec { 'flask':
-  command   => 'pip3 install flask==2.1.0',
-  path      => '/usr/bin',
-  unliss    => 'pip3 list | grep flask',
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
