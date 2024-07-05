d96ce6482db5192686e64cf3a081ae7d1fdfe520
Amro Sati <amrosatti98@gmail.com>
2024-07-05 16:40:11 +0300

    fixed

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,68 @@
 # Install and configure nginx server
 
-exec { 'update index':
-  command => '/usr/bin/apt-get update',
+exec { 'nginx-repo':
+  command => 'sudo add-apt-repository ppa:nginx/stable',
+  path    => '/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin',
+}
+
+exec { 'update-index':
+  command => 'apt-get update',
+  path    => '/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin',
 }
 
 package { 'nginx':
   ensure  => installed,
-  require => Exec['update index'],
+  require => Exec['nginx-repo', 'update index'],
+}
+
+exec { 'HTTP-allow':
+  command => "ufw allow 'Nginx HTTP'",
+  path    => '/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin',
+  onlyif  => '! dpkg -l nginx | egrep \'îi.*nginx\' > /dev/null 2>&1',
+}
+
+exec { '/var/www-permissions':
+  command => 'chmod -R 755 /var/www',
+  path    => '/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin',
 }
 
 file { '/var/www/html/index.html':
-  content => 'Hello World!',
-  require => Package['nginx'],
+  content => "Hello World!\n",
 }
 
-exec { 'redirect_me':
-  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.google.com permanent;" /etc/nginx/sites-available/default'
-  provider => 'shell',
+file { '/var/www/html/404.html':
+  content => "Ceci n'est pas une page \n"
+}
+
+file { 'default-config':
+  ensure  => file,
+  path    => '/etc/nginx/sites-enabled/default',
+  content =>
+"server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+		root /var/www/html;
+	index index.html index.htm index.nginx-debian.html;
+	server_name _;
+	location / {
+		try_files \$uri \$uri/ =404;
+	}
+
+	error_page 404 /404.html;
+	location /404.html {
+		internal;
+	}
+
+	if (\$request_filename ~ redirect_me) {
+		rewrite ^ https://www.google.com permanent;
+	}
+}
+",
+}
+
+exec { 'restart-nginx':
+  command => 'sudo service nginx restart',
+  path    => '/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin',
 }
 
 service { 'nginx':
