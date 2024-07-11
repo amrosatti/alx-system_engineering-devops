5667493b86623183a0198ebe1bc3e83cf05125cc
Amro Sati <amrosatti98@gmail.com>
2024-07-11 19:23:18 +0300

    task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,8 @@
 # Appends a custom HTTP response header
-exec { 'Nginx':
-  command  => 'sudo apt-get -y update;
-  sudo apt-get -y install nginx;
-  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-availale/default;
-  sudo service nginx restart',
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
   provider => shell,
 }
